d4e20850f3d528d0b946f781e349ef00f12f55f8
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-06-02 18:47:36 -0400

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,6 +4,7 @@ import badRequest from '../helpers/http-helper';
 import IController from '../protocols/icontroller';
 import IEmailValidator from '../protocols/iemail-validator';
 import InvalidParamError from '../erros/invalid-param-error';
+import ServerError from '../erros/server-error';
 
 class SignUpController implements IController {
   private readonly emailValidator;
@@ -13,22 +14,26 @@ class SignUpController implements IController {
   }
 
   handle(httpRequest: IHttpRequest): IHttpResponse {
-    const requeridFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ];
-    for (const field of requeridFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requeridFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
+      for (const field of requeridFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+
+      const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
+      if (!isValidEmail) return badRequest(new InvalidParamError('email'));
+
+      return { statusCode: 200, body: '' };
+    } catch (error) {
+      return { statusCode: 500, body: new ServerError() };
     }
-
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
-    if (!isValidEmail) return badRequest(new InvalidParamError('email'));
-
-    return { statusCode: 200, body: '' };
   }
 }
 
