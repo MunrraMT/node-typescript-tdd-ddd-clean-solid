0b92927121a76ca7fc33e36004b041049e96fd7c
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-06-06 07:24:32 -0400

    feat: ensure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -28,6 +28,10 @@ class SignUpController implements IController {
         }
       }
 
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
+
       const isValidEmail = this.emailValidator.isValid(httpRequest.body.email);
       if (!isValidEmail) return badRequest(new InvalidParamError('email'));
 
