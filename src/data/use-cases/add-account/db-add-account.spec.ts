1735d3d3046f535703f573cda34e67b936726c3b
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-06-07 16:33:34 -0400

    refactor: move sut creation to a factor helper method

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -1,21 +1,38 @@
+import InterfaceEncrypter from '~/data/protocols/interface-encrypter';
 import DbAddAccount from './db-add-account';
+
+type SutTypes = {
+  sut: DbAddAccount;
+  stubEncrypter: InterfaceEncrypter;
+};
+
+const makeSut = (): SutTypes => {
+  class StubEncrypter {
+    async encrypt(value: string): Promise<string> {
+      return Promise.resolve('hashed_password');
+    }
+  }
+  const stubEncrypter = new StubEncrypter();
+  const sut = new DbAddAccount(stubEncrypter);
+
+  return {
+    sut,
+    stubEncrypter,
+  };
+};
 
 describe('DbAddAccount UseCase', () => {
   test('Should call Encrypter with correct password', async () => {
-    class StubEncrypter {
-      async encrypt(value: string): Promise<string> {
-        return Promise.resolve('hashed_password');
-      }
-    }
-    const stubEncrypter = new StubEncrypter();
-    const sut = new DbAddAccount(stubEncrypter);
+    const { stubEncrypter, sut } = makeSut();
     const encryptSpy = jest.spyOn(stubEncrypter, 'encrypt');
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
       password: 'valid_password',
     };
+
     await sut.add(accountData);
+
     expect(encryptSpy).toHaveBeenCalledWith('valid_password');
   });
 });
