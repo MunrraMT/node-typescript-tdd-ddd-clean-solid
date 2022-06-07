0e5e8ecbfc5ea051f3430421ea644946b0c147a3
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-06-07 16:01:36 -0400

    test: ensure AdapterEmailValidator return true if validator returns true

--- src/utils/adapter-email-validator.spec.ts
+++ src/utils/adapter-email-validator.spec.ts
@@ -1,9 +1,24 @@
+import validator from 'validator';
+
 import AdapterEmailValidator from './adapter-email-validator';
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
 
 describe('AdapterEmailValidator', () => {
   test('Should returns false if validator returns false', () => {
     const sut = new AdapterEmailValidator();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_email@mail.com');
     expect(isValid).toBe(false);
+  });
+
+  test('Should returns true if validator returns true', () => {
+    const sut = new AdapterEmailValidator();
+    const isValid = sut.isValid('valid_email@mail.com');
+    expect(isValid).toBe(true);
   });
 });
