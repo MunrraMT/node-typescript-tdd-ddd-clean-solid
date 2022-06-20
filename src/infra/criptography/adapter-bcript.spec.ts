1418d7bdaa0cd0e7ffeeced8aba951e1fe6645ba
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-06-20 08:58:24 -0400

    test: ensure AdapterBcrypter returns a hash on success

--- src/infra/criptography/adapter-bcript.spec.ts
+++ src/infra/criptography/adapter-bcript.spec.ts
@@ -2,6 +2,12 @@ import bcrypt from 'bcrypt';
 
 import AdapterBcrypter from './adapter-bcrypt';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve('hash_value');
+  },
+}));
+
 describe('Bcrypter Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const salt = 12;
@@ -9,5 +15,12 @@ describe('Bcrypter Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(bcrypt.hash).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('Should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new AdapterBcrypter(salt);
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hash_value');
   });
 });
